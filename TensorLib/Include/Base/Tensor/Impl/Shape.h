2e78c2697f0c4581a0c20e996640cf7066a97d95
纳鲁赛繆·尤梅米·希娜卡纳 <1751842477@qq.com>
2025-06-04 17:50:59 +0800

    Refactor Shape ops

--- TensorLib/Include/Base/Tensor/Impl/Shape.h
+++ TensorLib/Include/Base/Tensor/Impl/Shape.h
@@ -90,6 +90,391 @@ constexpr decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::ViewDimensions(
 		Ret._MyShape[0] = 1;
 		Ret._MyViewStride[0] = _MyViewStride.Back();
 	}
+
+	_D_Dragonian_Lib_Auto_Grad(Index, *this, _Indice, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _SliceDim, typename _FirstType, typename... _ArgTypes>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::operator()(
+	_FirstType _Index,
+	_ArgTypes... _Args
+	) const requires ((sizeof...(_ArgTypes) < _NRank) && TypeTraits::IsIntegerValue<_FirstType> && (_SliceDim < _NRank))
+{
+	if constexpr (TypeTraits::IsStringValue<_FirstType>)
+		return operator() < _SliceDim > (Range(_Index), _Args...);
+	else if constexpr (_SliceDim)
+	{
+		_Index = static_cast<_FirstType>(CalcIndex(static_cast<SizeType>(_Index), _MyShape[_SliceDim]));
+		return operator() < _SliceDim > (Range{ static_cast<SizeType>(_Index), static_cast<SizeType>(_Index + 1) }, _Args...);
+	}
+	else if constexpr (_NRank == 1)
+		return Get(static_cast<SizeType>(_Index));
+	else if constexpr (sizeof...(_ArgTypes))
+		return operator[](static_cast<SizeType>(_Index)).template operator() < 0 > (_Args...);
+	else
+		return operator[](static_cast<SizeType>(_Index));
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _SliceDim, typename ... _ArgTypes>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::operator()(
+	Range _Range,
+	_ArgTypes... _Args
+	) const requires ((sizeof...(_ArgTypes) < _NRank) && (_SliceDim < _NRank))
+{
+	SliceOptions<_NRank> SliceOptions;
+	SliceOptions[_SliceDim] = _Range;
+	if constexpr (sizeof...(_ArgTypes))
+		return Slice(SliceOptions).template operator() < _SliceDim + 1 > (_Args...);
+	else
+		return Slice(SliceOptions);
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _Count>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Split(
+	const Dimensions<_Count>& _Size,
+	SizeType _Axis
+) const
+{
+	ThrowOnNotEnabled();
+	IDLArray<Tensor, _Count> Ret;
+	_Axis = CalcIndex(_Axis, Rank());
+	const auto _SizeTgr = _MyShape[_Axis];
+	const auto _SizeSrc = _Size.Multiply();
+
+	if (_SizeTgr != _SizeSrc)
+		_D_Dragonian_Lib_Throw_Exception("The Size Of The Split Is Not Equal To The Source!");
+
+	SliceOptions<_Count> SliceOptions;
+	SliceOptions[_Axis] = { 0, 1, 0 };
+	for (size_t i = 0; i < _Count; ++i)
+	{
+		SliceOptions[_Axis].End += _Size[i];
+		Ret[i] = Slice(SliceOptions);
+		SliceOptions[_Axis].Begin += _Size[i];
+	}
+
+	_D_Dragonian_Lib_Auto_Grad(Split, *this, _Size, _Axis, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Split(
+	SizeType _Size,
+	SizeType _Axis
+) const
+{
+	ThrowOnNotEnabled();
+	TemplateLibrary::Vector<Tensor> Ret;
+	_Axis = CalcIndex(_Axis, Rank());
+
+	if (_Size <= 0)
+		_D_Dragonian_Lib_Throw_Exception("The Size Of The Split Is Not Valid!");
+
+	auto Remain = _MyShape[_Axis];
+	Ret.Reserve(Remain / _Size + 1);
+
+	SliceOptions<_NRank> SliceOptions;
+	SliceOptions[_Axis] = { 0, 1, 0 };
+	while (Remain)
+	{
+		const auto CSize = std::min(_Size, Remain);
+		SliceOptions[_Axis].End += CSize;
+		Ret.EmplaceBack(Slice(SliceOptions));
+		SliceOptions[_Axis].Begin += CSize;
+		Remain -= CSize;
+	}
+
+	_D_Dragonian_Lib_Auto_Grad(Split, *this, _Size, _Axis, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _Rnk>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Slice(
+	const SliceOptions<_Rnk>& _SliceOptions
+) const requires (_Rnk <= _NRank && _Rnk > 0)
+{
+	ThrowOnNotEnabled();
+	if (IsBroadCasted())
+		_D_Dragonian_Lib_Throw_Exception("Broad Casted Could Not Be Sliced!");
+
+	Tensor Ret = View();
+	for (size_t i = 0; i < _Rnk; ++i)
+	{
+		if (_SliceOptions[i].Begin == 0 &&
+			_SliceOptions[i].Step == 1 &&
+			(_SliceOptions[i].End == RangeEndPos || _SliceOptions[i].End == _MyShape[i]))
+			continue;
+
+		SizeType SliceBeginPos, SliceStep, SliceEndPos;
+
+		if (_SliceOptions[i].Begin == _SliceOptions[i].Step && _SliceOptions[i].Begin == _SliceOptions[i].End)
+		{
+			SliceBeginPos = CalcIndex(_SliceOptions[i].Step, _MyShape[i], false);
+			SliceStep = 1;
+			SliceEndPos = SliceBeginPos + 1;
+		}
+		else
+		{
+			SliceStep = _SliceOptions[i].Step;
+			if (SliceStep == 0)
+				_D_Dragonian_Lib_Throw_Exception("SliceStep Should Not Be Zero!");
+			SliceBeginPos = CalcIndex(_SliceOptions[i].Begin, _MyShape[i], false);
+			SliceEndPos = CalcEndPos(_SliceOptions[i].End, _MyShape[i], false);
+		}
+
+		const auto SliceLength = SliceEndPos - SliceBeginPos;
+		if (SliceLength == 0)
+			_D_Dragonian_Lib_Throw_Exception("SliceLength could not be zero!");
+		if (SliceLength / SliceStep < 0)
+			_D_Dragonian_Lib_Throw_Exception("Shape could not be negative!");
+		const auto SlicedShape = Ceil(abs(SliceLength), abs(SliceStep));
+
+		Ret._MyData += SliceBeginPos * Ret._MyViewStride[i];
+		Ret._MyShape[i] = SlicedShape;
+		Ret._MyViewStride[i] *= SliceStep;
+	}
+
+	_D_Dragonian_Lib_Auto_Grad(Slice, *this, _SliceOptions, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _Rnk>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::ReversedSlice(
+	const SliceOptions<_NRank>& _SliceOptions
+) const requires (_Rnk <= _NRank && _Rnk > 0)
+{
+	ThrowOnNotEnabled();
+	if (IsBroadCasted())
+		_D_Dragonian_Lib_Throw_Exception("Broad Casted Could Not Be Sliced!");
+
+	Tensor Ret = View();
+	for (size_t j = 0; j < _Rnk; ++j)
+	{
+		const auto i = _Rnk - 1 - j; // Reverse the index for reversed slice
+
+		if (_SliceOptions[j].Begin == 0 &&
+			_SliceOptions[j].Step == 1 &&
+			(_SliceOptions[j].End == RangeEndPos || _SliceOptions[j].End == _MyShape[i]))
+			continue;
+
+		SizeType SliceBeginPos, SliceStep, SliceEndPos;
+
+		if (_SliceOptions[j].Begin == _SliceOptions[j].Step && _SliceOptions[j].Begin == _SliceOptions[j].End)
+		{
+			SliceBeginPos = CalcIndex(_SliceOptions[j].Step, _MyShape[i], false);
+			SliceStep = 1;
+			SliceEndPos = SliceBeginPos + 1;
+		}
+		else
+		{
+			SliceStep = _SliceOptions[j].Step;
+			if (SliceStep == 0)
+				_D_Dragonian_Lib_Throw_Exception("SliceStep Should Not Be Zero!");
+			SliceBeginPos = CalcIndex(_SliceOptions[j].Begin, _MyShape[i], false);
+			SliceEndPos = CalcEndPos(_SliceOptions[j].End, _MyShape[i], false);
+		}
+
+		const auto SliceLength = SliceEndPos - SliceBeginPos;
+		if (SliceLength == 0)
+			_D_Dragonian_Lib_Throw_Exception("SliceLength could not be zero!");
+		if (SliceLength / SliceStep < 0)
+			_D_Dragonian_Lib_Throw_Exception("Shape could not be negative!");
+		const auto SlicedShape = Ceil(abs(SliceLength), abs(SliceStep));
+
+		Ret._MyData += SliceBeginPos * Ret._MyViewStride[i];
+		Ret._MyShape[i] = SlicedShape;
+		Ret._MyViewStride[i] *= SliceStep;
+	}
+
+	_D_Dragonian_Lib_Auto_Grad(ReverseSlice, *this, _SliceOptions, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Permute(
+	const Dimensions<_NRank>& _PremuteOrder
+) const
+{
+	ThrowOnNotEnabled();
+	if (_MyShape.Empty() || _PremuteOrder.Size() != _MyShape.Size())
+		_D_Dragonian_Lib_Throw_Exception("N_DIMS MisMatch!");
+	Tensor Ret = View();
+	Dimensions<_NRank> TransposedDims = _PremuteOrder;
+	std::ranges::sort(TransposedDims);
+	if (TransposedDims[0] != 0)
+		_D_Dragonian_Lib_Throw_Exception("DPremute Must Have [0, 1, ... , N_DIMS - 1]!");
+	for (size_t i = 1; i < TransposedDims.Size(); ++i)
+		if (TransposedDims[i] != TransposedDims[i - 1] + 1)
+			_D_Dragonian_Lib_Throw_Exception("DPremute Must Have [0, 1, ... , N_DIMS - 1]!");
+
+	for (size_t i = 0; i < _PremuteOrder.Size(); ++i)
+	{
+		Ret._MyShape[i] = _MyShape[_PremuteOrder[i]];
+		Ret._MyViewStride[i] = _MyViewStride[_PremuteOrder[i]];
+	}
+
+	_D_Dragonian_Lib_Auto_Grad(Permute, *this, _PremuteOrder, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Transpose(
+	SizeType _Axis1,
+	SizeType _Axis2
+) const
+{
+	ThrowOnNotEnabled();
+	const auto AxisCount = (SizeType)_MyShape.Size();
+	_Axis1 = CalcIndex(_Axis1, AxisCount);
+	_Axis2 = CalcIndex(_Axis2, AxisCount);
+	Tensor Ret = View();
+	if (_Axis1 == _Axis2)
+		return Ret;
+	Ret._MyShape[_Axis2] = _MyShape[_Axis1];
+	Ret._MyViewStride[_Axis2] = _MyViewStride[_Axis1];
+	Ret._MyShape[_Axis1] = _MyShape[_Axis2];
+	Ret._MyViewStride[_Axis1] = _MyViewStride[_Axis2];
+
+	_D_Dragonian_Lib_Auto_Grad(Permute, *this, _Axis1, _Axis2, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::AxisFromTo(
+	SizeType _Begin,
+	SizeType _End
+) const
+{
+	ThrowOnNotEnabled();
+	const auto AxisCount = (SizeType)_MyShape.Size();
+	_Begin = CalcIndex(_Begin, AxisCount);
+	_End = CalcIterator(_End, AxisCount);
+	if (_Begin > _End)
+		_D_Dragonian_Lib_Throw_Exception("Begin Should Not Be Greater Than End!");
+	Tensor Ret = View();
+	for (SizeType i = _Begin; i < _End - 1; ++i)
+	{
+		std::swap(Ret._MyShape[i], Ret._MyShape[i + 1]);
+		std::swap(Ret._MyViewStride[i], Ret._MyViewStride[i + 1]);
+	}
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::UnSqueeze(
+	SizeType _Dim
+) const
+{
+	ThrowOnNotEnabled();
+	Tensor<_TensorType, _NRank + 1, _MyDevice> Ret;
+	_Dim = CalcIterator(_Dim, Rank());
+	const auto _Value = _Dim == Rank() ? 1 : _MyViewStride[_Dim] * _MyShape[_Dim];
+	Ret._MyShape = _MyShape.Insert(1, _Dim);
+	Ret._MyViewStride = _MyViewStride.Insert(_Value, _Dim);
+	Ret._MyFirst = _MyFirst;
+	Ret._MyData = _MyData;
+	Ret._MyLast = _MyLast;
+	Ret._MyFuturesAsResult = _MyFuturesAsResult;
+	Ret._MyFuturesAsArgument = _MyFuturesAsArgument;
+	Ret._MyAllocator = _MyAllocator;
+	Ret._IgnoreDep = _IgnoreDep;
+	Ret._MyGraph = _MyGraph;
+	Ret._MyFunction = _MyFunction;
+
+	_D_Dragonian_Lib_Auto_Grad(UnSqueeze, *this, _Dim, Ret);
+	return Ret;
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::Squeeze(
+	SizeType _Dim
+) const
+{
+	ThrowOnNotEnabled();
+	if constexpr (_NRank == 1)
+		return View();
+	else
+	{
+		_Dim = CalcIndex(_Dim, SizeType(_MyShape.Size()));
+		if (_MyShape[_Dim] != 1)
+			_D_Dragonian_Lib_Throw_Exception("The Shape Of Dim Must Be 1!");
+		Tensor<_TensorType, _NRank - 1, _MyDevice> Ret;
+		Ret._MyShape = _MyShape.Erase(_Dim);
+		Ret._MyViewStride = _MyViewStride.Erase(_Dim);
+		Ret._MyFirst = _MyFirst;
+		Ret._MyData = _MyData;
+		Ret._MyLast = _MyLast;
+		Ret._MyFuturesAsResult = _MyFuturesAsResult;
+		Ret._MyFuturesAsArgument = _MyFuturesAsArgument;
+		Ret._MyAllocator = _MyAllocator;
+		Ret._IgnoreDep = _IgnoreDep;
+		Ret._MyGraph = _MyGraph;
+		Ret._MyFunction = _MyFunction;
+
+		_D_Dragonian_Lib_Auto_Grad(Squeeze, *this, _Dim, Ret);
+		return Ret;
+	}
+}
+
+template <typename _TensorType, size_t _NRank, Device _MyDevice>
+template <size_t _TRank>
+decltype(auto) Tensor<_TensorType, _NRank, _MyDevice>::View(
+	const Dimensions<_TRank>& _ViewShape
+) const
+{
+	ThrowOnNotEnabled();
+
+	if (!IsContiguous())
+		_D_Dragonian_Lib_Throw_Exception("View Should Be Contiguous!");
+	const auto DynamicCount = std::ranges::count(_ViewShape, -1);
+	if (DynamicCount > 1)
+		_D_Dragonian_Lib_Throw_Exception("Count Of Dynamic Axis Should Be <= 1!");
+	bool HasDynamic = DynamicCount == 1;
+	for (const auto i : _ViewShape)
+		if (i <= 0 && i != -1)
+			_D_Dragonian_Lib_Throw_Exception("Count Of Size Should Be Greater Than 0 Or Equal -1 (Dynamic Axis)!");
+
+	const auto SrcSize = _MyShape.Multiply();
+	const auto DstSize = std::abs(_ViewShape.Multiply());
+
+	const auto Remainder = SrcSize % DstSize;
+	const auto DynamicAxes = SrcSize / DstSize;
+
+	if (Remainder || (!HasDynamic && DynamicAxes != 1))
+		_D_Dragonian_Lib_Throw_Exception("Could Not View The Tensor With Size["
+			+ std::to_string(SrcSize) + "] To Size[" + std::to_string(DstSize) + "]!");
+
+	Tensor<_TensorType, _TRank, _MyDevice> Ret;
+	Ret._MyShape = _ViewShape;
+	if (DynamicAxes >= 1)
+		*std::ranges::find(Ret._MyShape, -1) = DynamicAxes;
+	auto _Begin = Ret._MyViewStride.ReversedBegin();
+	const auto _End = Ret._MyViewStride.ReversedEnd();
+	auto _Iter = Ret._MyShape.ReversedBegin();
+	*_Begin-- = 1;
+	while (_Begin != _End)
+	{
+		*_Begin = *(_Begin + 1) * *_Iter--;
+		--_Begin;
+	}
+
+	Ret._MyFirst = _MyFirst;
+	Ret._MyData = _MyData;
+	Ret._MyLast = _MyLast;
+	Ret._MyFuturesAsResult = _MyFuturesAsResult;
+	Ret._MyFuturesAsArgument = _MyFuturesAsArgument;
+	Ret._MyAllocator = _MyAllocator;
+	Ret._IgnoreDep = _IgnoreDep;
+	Ret._MyGraph = _MyGraph;
+	Ret._MyFunction = _MyFunction;
+
+	_D_Dragonian_Lib_Auto_Grad(View, *this, _ViewShape, Ret);
 	return Ret;
 }
 
